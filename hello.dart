2269539bede803b22eb78abcb92fbc6cf37d5f4d
Pukar Nepali <akaszn16@gmail.com>
2024-06-03 11:34:35 +0545

    Variables

--- hello.dart
+++ hello.dart
@@ -1,3 +1,9 @@
 void main() {
   print("Hello World");
+
+  String Name = "Ram"; // declaration and initialization
+
+  String? personName; //declaration
+
+  personName = "Ram"; // initilization
 }
