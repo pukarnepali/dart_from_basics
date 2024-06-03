2beaddd8a7127fd33a54014bf5758df672a15d95
Pukar Nepali <akaszn16@gmail.com>
2024-06-03 12:11:05 +0545

    first dart program

--- hello.dart
+++ hello.dart
@@ -1,9 +1,52 @@
 void main() {
-  print("Hello World");
+  String name = "Pukar Nepali";
+  String address = "Pokhara";
+  int phoneNumber = 9800000000;
+  bool isMarried = false;
+  String educationQualification = "BSc. IT";
+  double height = 5.6;
+  int weight = 71;
 
-  String Name = "Ram"; // declaration and initialization
+  print(
+      "My Name is $name.  address is $address.  phone number is $phoneNumber.  \n height is $height.  weight is $weight.  Education Qualification is $educationQualification.  Marritial Status $isMarried.");
+  // // name, address, phone number , marritial status, education qualification, height, weight
+  // print("Hello World");
 
-  String? personName; //declaration
+  // // string
 
-  personName = "Ram"; // initilization
+  // String Name = "Ram"; // declaration and initialization
+
+  // String? personName; //declaration
+
+  // String personBio = """Hi i am Pukar Nepali. I am writing
+
+  // three line bio"""; // use """
+
+  // String randomSentence = " \" Look at this \" 'person' ";
+
+  // personName = "Ram"; // initilization
+
+  // // Integer, Double, Boolean
+
+  // int phoneNumber = 98000000;
+  // double height = 5.6;
+  // bool isMarried = true;
 }
+
+// data type
+
+// 1. String => 'String', "String", """String"""
+// 2. Integers int => 1, -100, 100
+// 3. Double double => 2.4, -2.4
+// 4. Number number => integer+ double ko combination
+// 5. Boolean bool => true /false
+
+// Lower camel case => highSchoolTeacherName, noOfVehicles, personName > used for Variables and Functions Name
+// Uppr camel case => HighSchoolTeacherName, NoOfVehicles, PersonName > used for Class Name
+// snake case => high_school_teacher_name, no_of_vehicles, person_name > used for File Name
+
+// Comments
+
+// single line comments => // My Name is Pukar Nepali.
+// multi line comments => /* My Name is Pukar Nepali. */
+// documentation comments => /// use of documentation comments
